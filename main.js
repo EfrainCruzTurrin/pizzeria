f899b1ed94aa259cc85ca005bcff6ab47dce6f89
Ramiro Hernandez <ramiroh.dev@gmail.com>
2025-11-15 12:16:11 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("PizzaLine - Sitio cargado correctamente 🍕");
+
+    const btn = document.getElementById("btn-flotante");
+    btn.addEventListener("click", () => {
+        alert("Próximamente: carrito de compras 🛒");
+    });
+});
